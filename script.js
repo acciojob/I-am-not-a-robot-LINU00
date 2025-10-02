67efe54d257e7ffa348b264c87bcbe64a98900d4
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-10-03 04:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const images = ["img1", "img2", "img3", "img4", "img5"];
-let selectedImages = [];
-const imageContainer = document.getElementById("image-container");
-const resetBtn = document.getElementById("reset");
-const verifyBtn = document.getElementById("verify");
-const para = document.getElementById("para");
+var images = ["img1", "img2", "img3", "img4", "img5"];
+var selectedImages = [];
+var imageContainer = document.getElementById("image-container");
+var resetBtn = document.getElementById("reset");
+var verifyBtn = document.getElementById("verify");
+var para = document.getElementById("para");
 
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return array;
 }
@@ -19,15 +21,17 @@ function setupImages() {
   selectedImages = [];
   resetBtn.style.display = "none";
   verifyBtn.style.display = "none";
-  let imgs = [...images];
-  const duplicateIndex = Math.floor(Math.random() * imgs.length);
+  var imgs = images.slice();
+  var duplicateIndex = Math.floor(Math.random() * imgs.length);
   imgs.push(imgs[duplicateIndex]);
   imgs = shuffle(imgs);
-  imgs.forEach((imgClass, index) => {
-    const img = document.createElement("img");
+  imgs.forEach(function(imgClass) {
+    var img = document.createElement("img");
     img.className = imgClass;
     img.dataset.id = imgClass;
-    img.addEventListener("click", () => handleClick(img));
+    img.addEventListener("click", function() {
+      handleClick(img);
+    });
     imageContainer.appendChild(img);
   });
 }
@@ -42,72 +46,13 @@ function handleClick(img) {
   }
 }
 
-resetBtn.addEventListener("click", () => {
+resetBtn.addEventListener("click", function() {
   setupImages();
 });
 
-verifyBtn.addEventListener("click", () => {
-  const [img1, img2] = selectedImages;
-  if (img1.dataset.id === img2.dataset.id) {
-    para.textContent = "You are a human. Congratulations!";
-  } else {
-    para.textContent = "We can't verify you as a human. You selected the non-identical tiles.";
-  }
-  verifyBtn.style.display = "none";
-});
-
-window.onload = setupImages;
-//your code here
-const images = ["img1", "img2", "img3", "img4", "img5"];
-let selectedImages = [];
-const imageContainer = document.getElementById("image-container");
-const resetBtn = document.getElementById("reset");
-const verifyBtn = document.getElementById("verify");
-const para = document.getElementById("para");
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-function setupImages() {
-  imageContainer.innerHTML = "";
-  para.textContent = "";
-  selectedImages = [];
-  resetBtn.style.display = "none";
-  verifyBtn.style.display = "none";
-  let imgs = [...images];
-  const duplicateIndex = Math.floor(Math.random() * imgs.length);
-  imgs.push(imgs[duplicateIndex]);
-  imgs = shuffle(imgs);
-  imgs.forEach((imgClass, index) => {
-    const img = document.createElement("img");
-    img.className = imgClass;
-    img.dataset.id = imgClass;
-    img.addEventListener("click", () => handleClick(img));
-    imageContainer.appendChild(img);
-  });
-}
-
-function handleClick(img) {
-  if (selectedImages.includes(img)) return;
-  if (selectedImages.length < 2) {
-    img.classList.add("selected");
-    selectedImages.push(img);
-    resetBtn.style.display = "inline-block";
-    if (selectedImages.length === 2) verifyBtn.style.display = "inline-block";
-  }
-}
-
-resetBtn.addEventListener("click", () => {
-  setupImages();
-});
-
-verifyBtn.addEventListener("click", () => {
-  const [img1, img2] = selectedImages;
+verifyBtn.addEventListener("click", function() {
+  var img1 = selectedImages[0];
+  var img2 = selectedImages[1];
   if (img1.dataset.id === img2.dataset.id) {
     para.textContent = "You are a human. Congratulations!";
   } else {
